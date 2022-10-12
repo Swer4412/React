8d1edac0154f0691e94ef8dd672457919b3cfdb4
Swer4412 <109923930+Swer4412@users.noreply.github.com>
2022-10-12 11:57:44 +0200

    cumcast

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import React from 'react';//La funzione di react è quella di TROVARE LE DIFFERE
 import ReactDOM from 'react-dom/client'; //React una volta capiti i cambiamenti, deve saper renderizzarli 
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-
-
+//React query
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools} from 'react-query/devtools'
 
 /*
 import App from './components/App';
@@ -17,13 +18,15 @@ import abc, {add, sub} from './calculator.js'
 console.log(add(1,3))
 console.log(sub(1,3))
 
-
+const client = new QueryClient()
 // Per il funzionamento di una app react basta avere una struttura(index.html), un file che renderizza su quella struttura (index.js), tutto questo grazie ad un bundler
-
 const root = ReactDOM.createRoot(document.getElementById('root')); //Essendo react 18.x si scrive in questo modo
 // Nel javascript normale non si puo mischiare html e css, c'è per questo create-react-app (bundler) che inietta del codice in modo che funzioni
 root.render( 
-  <TailWind/>
+  <QueryClientProvider client = {client}>
+    <TailWind/>
+    <ReactQueryDevtools/>
+  </QueryClientProvider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
